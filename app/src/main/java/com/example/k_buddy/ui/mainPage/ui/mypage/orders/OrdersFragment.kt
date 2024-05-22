fa5de679a58c882829e25d0c94fe257fce52dd9b
brightalot <gamja9151@hanyang.ac.kr>
2024-05-23 01:58:13 +0900

    [feat][Add OrdersAdapter, OrdersItem and Improve OrdersFragment layout design][brightalot]

--- app/src/main/java/com/example/k_buddy/ui/mainPage/ui/mypage/orders/OrdersFragment.kt
+++ app/src/main/java/com/example/k_buddy/ui/mainPage/ui/mypage/orders/OrdersFragment.kt
@@ -6,27 +6,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.k_buddy.R
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.k_buddy.databinding.FragmentOrdersBinding
 
 class OrdersFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = OrdersFragment()
-    }
+    private var _binding: FragmentOrdersBinding? = null
+    private val binding get() = _binding!!
 
-    private lateinit var viewModel: OrdersViewModel
-
+    // ViewModel을 by viewModels()를 사용하여 초기화
+    private val viewModel: OrdersViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_orders, container, false)
+    ): View {
+        _binding = FragmentOrdersBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(OrdersViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.adapter = OrdersAdapter(viewModel.items)
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
