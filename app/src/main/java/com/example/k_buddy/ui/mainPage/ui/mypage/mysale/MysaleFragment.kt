cf06cfda3cf22f268bd1884d9e3244c394c4e838
brightalot <gamja9151@hanyang.ac.kr>
2024-05-24 02:58:31 +0900

    [feat][Improve mysaleActivity layout][brightalot]

--- app/src/main/java/com/example/k_buddy/ui/mainPage/ui/mypage/mysale/MysaleFragment.kt
+++ app/src/main/java/com/example/k_buddy/ui/mainPage/ui/mypage/mysale/MysaleFragment.kt
@@ -1,20 +1,19 @@
 package com.example.k_buddy.ui.mainPage.ui.mypage.mysale
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.example.k_buddy.R
 import com.example.k_buddy.databinding.FragmentMysaleBinding
-import com.google.android.material.tabs.TabLayoutMediator
 
 class MysaleFragment : Fragment() {
 
     private var _binding: FragmentMysaleBinding? = null
     private val binding get() = _binding!!
 
-    // ViewModel을 by viewModels()를 사용하여 초기화
     private val viewModel: MysaleViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,23 +25,28 @@ class MysaleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupViewPagerAndTabs()
+        setupViewPager()
+        setupButtons()
     }
-    private fun setupViewPagerAndTabs() {
-        // ViewPager 설정
+
+    private fun setupViewPager() {
         val viewPager = binding.mySaleViewPager
-        val tabLayout = binding.mySaleTabLayout
         val adapter = MysaleAdapter(requireActivity())
         viewPager.adapter = adapter
+    }
 
-        // TabLayout과 ViewPager 연결
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = when (position) {
-                0 -> "Sales"
-                1 -> "Listings"
-                else -> null
-            }
-        }.attach()
+    private fun setupButtons() {
+        binding.buttonSales.setOnClickListener {
+            binding.mySaleViewPager.currentItem = 0
+            binding.buttonSales.setBackgroundResource(R.drawable.mysale_clicked_button)
+            binding.buttonListings.setBackgroundResource(R.drawable.mysale_unclicked_button)
+        }
+
+        binding.buttonListings.setOnClickListener {
+            binding.mySaleViewPager.currentItem = 1
+            binding.buttonSales.setBackgroundResource(R.drawable.mysale_unclicked_button)
+            binding.buttonListings.setBackgroundResource(R.drawable.mysale_clicked_button)
+        }
     }
 
     override fun onDestroyView() {
