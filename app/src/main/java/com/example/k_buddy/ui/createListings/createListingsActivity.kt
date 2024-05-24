4db04e6ff052755fabd394927e9a85a982d3bfe5
brightalot <gamja9151@hanyang.ac.kr>
2024-05-24 15:51:12 +0900

    [feat][Add createListingsActivity][brightalot]

---
+++ app/src/main/java/com/example/k_buddy/ui/createListings/createListingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.k_buddy.ui.createListings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.k_buddy.R
+
+class createListingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_listings)
+    }
+}
